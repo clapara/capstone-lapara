b88ef41aff1265186df65a5f7eace492b02b6717
Cailley LaPara <cailley.lapara67@journalism.cuny.edu>
2022-12-06 19:59:37 -0500

    added video iframe

--- main.js
+++ main.js
@@ -21,3 +21,38 @@ const images = document.querySelectorAll(".collage > .img");
             
         });
     });
+
+function onYouTubeIframeAPIReady() {
+    const video_player = document.querySelector(".yt-player");
+    const video_id = video_player.dataset.videoId;
+    player = new YT.Player(video_player, {
+      height: "390",
+      width: "640",
+      videoId: video_id,
+      playerVars: {
+        'playsinline': 1,
+        'controls': 0,
+        'modestbranding': 1,
+        'showinfo': 0
+      },
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onStateChange
+      }
+    });
+}
+
+function onPlayerReady(e){
+    console.log(">> READY >>", e)
+
+    e.target.mute();
+    e.target.playVideo();
+}
+
+function onStateChange(e){
+    if(e.data === YT.PlayerState.PLAYING) {
+        setTimeout(() => {
+            e.target.getIframe().closest(".video-frame").classList.add("active");
+        }, 500);
+    }
+}
